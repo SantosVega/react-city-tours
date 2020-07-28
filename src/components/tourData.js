b77485a9e7731558bfb1dc7223f301f9cee36408
unknown <ezequielsantosvega@gmail.com>
2020-07-27 21:28:53 -0300

    first commit

---
+++ src/components/tourData.js
@@ -0,0 +1,74 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "Bariloche",
+    img: "./img/Bariloche.jpg",
+    name: "Downtown tour and Nature",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!",
+  },
+  {
+    id: 2,
+    city: "El Chaltén",
+    img: "./img/ElChalten.jpg",
+    name: "The Majesty of El Chaltén",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!",
+  },
+  {
+    id: 3,
+    city: "Puerto Madryn",
+    img: "./img/PuertoMadryn.jpg",
+    name: "Puerto Madryn's Whales",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!",
+  },
+  {
+    id: 4,
+    city: "Ushuaia",
+    img: "./img/Ushuaia.jpg",
+    name: "The Southernmost City in the World",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!",
+  },
+  {
+    id: 5,
+    city: "El Bolsón",
+    img: "./img/ElBolson.jpg",
+    name: "City of elves and fairies",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!",
+  },
+  {
+    id: 6,
+    city: "Esquel",
+    img: "./img/Esquel.jpg",
+    name: "At the foot of the mountain range",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!",
+  },
+  {
+    id: 7,
+    city: "Lago Puelo",
+    img: "./img/LagoPuelo.jpg",
+    name: "The blue and green lake",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!",
+  },
+  {
+    id: 8,
+    city: "San Antonio Oeste",
+    img: "./img/SanAntonioOeste.jpg",
+    name: "Nice and peaceful sea",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!",
+  },
+  {
+    id: 9,
+    city: "San Martín de los Andes",
+    img: "./img/SMA.jpeg",
+    name: "Another awesome city in the 7 Lagos Route",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!",
+  },
+];
